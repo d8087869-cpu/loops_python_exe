83c365824f551ea4eb90f14a6be4d9b40455a24f
david <d8087869@gmail.com>
2026-06-24 16:48:42 +0300

    added1-9

---
+++ loops.py
@@ -0,0 +1,70 @@
+#1
+'''
+i = 0
+while i <= 5: 
+    print(i)
+    i+=1
+#2
+i = 10
+while i>=0 :
+    print(i)    
+    i=i-1'''
+#3
+'''
+total = 0 
+while total< 10:
+    total+=1
+print(total)    
+#4
+items = [2, 4, 6, 8]
+i=0
+while i< len (items)-1:
+    print(items[i])
+    i+=1
+    if items[i]>5:
+        break
+#5
+i = 0 
+while i <=10:
+    if  i% 2 == 0: 
+        print(i)
+    i=i+1
+'''    
+
+#6  
+'''  
+agents = ['Alpha', 'Bravo', 'Charlie']
+i = 0 
+while  i < len(agents):
+    print(agents[i])
+    i+=1
+'''
+"""
+scores = {'Alpha': 80, 'Bravo': 95, 'Charlie': 70}
+i = 0
+while i  < len (scores)-1:
+    print(scores.i)
+"""
+#8
+'''
+i=1
+while i*2<100:
+    i=i*2
+print(i)
+'''
+#9
+'''
+data = [3, 7, 2, -1, 5]
+i=0
+total = 0
+while i < len(data):
+    if data[i]==-1:
+        break
+    total=total+data[i]
+    i=i+1
+print(total)
+'''
+n = 10
+i = 1
+while i<=10:
+    print(f'')
