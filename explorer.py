643ea76b6b63bb06c724e09acd177ba34978c13e
david <d8087869@gmail.com>
2026-06-25 14:50:20 +0300

    3-5

--- explorer.py
+++ explorer.py
@@ -1,6 +1,19 @@
+'''
 fruits = ['apple', 'banana', 'cherry']
 for items in fruits:
     print(items)
 #2    
 for i in range(1,6):
     print(i)
+#3 
+for i in range(0, 10, 2):
+    print(i)'''
+#4
+fruits = ['apple', 'banana', 'cherry']
+for index, fruit in enumerate(fruits):
+    print(f'{index}: {fruit}')
+#5
+scores = {'Alpha': 80, 'Bravo': 95}
+for name , score in scores.items():
+    print(f'{name} , {score}')
+    
